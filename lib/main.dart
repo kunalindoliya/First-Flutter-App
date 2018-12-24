e6e261e5c7f88de3993f82b13568b2001bcc7b8c
Kunal Indoliya <kunalindoliya@gmail.com>
2018-12-24 23:39:39 +0530

    Adding List.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatefulWidget {
 
 }
 class _MyAppState extends State<MyApp>{
+  List<String> _products=['Food Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,17 +23,23 @@ class _MyAppState extends State<MyApp>{
             Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      _products.add("Advanced Food Tester");
+                    });
+                  },
                   child: Text('Add Product'),
                 ),
             ),
-            Card(
+            Column(children:_products.map((element)=> Card(
               child: Column(
                 children: <Widget>[
                   Image.asset('assets/food.jpg'),
-                  Text("Food paradise")
+                  Text(element)
                 ],
               ),
+            ),
+            ).toList(),
             ),
           ])
       ),
