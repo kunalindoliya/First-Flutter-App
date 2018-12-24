285fef07a08d735595f1c677b7481a1e569791bd
Kunal Indoliya <kunalindoliya@gmail.com>
2018-12-24 23:19:42 +0530

    Changing to Stateful Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-   @override
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+
+}
+class _MyAppState extends State<MyApp>{
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My APP'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text("Food paradise")
-        ],
-        ),
-        ),
+          appBar: AppBar(
+            title: Text('My APP'),
+          ),
+          body: Column(children: [
+            Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text("Food paradise")
+                ],
+              ),
+            ),
+          ])
       ),
     );
   }
