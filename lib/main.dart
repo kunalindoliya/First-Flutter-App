bc281a6e90c6eda9eb453d3dbbde6e24d82b45a8
Kunal Indoliya <kunalindoliya@gmail.com>
2018-12-25 21:26:53 +0530

    Adding Material Design.

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        //brightness: Brightness.dark, for dark color
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('My APP'),
           ),
-          body:ProductManager(),
+          body:ProductManager("Food Tester"),
       ),
     );
   }
