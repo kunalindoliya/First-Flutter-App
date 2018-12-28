8dc94d040749e4f8b3f104b46f31acb54f18ad2f
Kunal Indoliya <kunalindoliya@gmail.com>
2018-12-28 22:12:06 +0530

    Adding TextFields.

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 
-class ProductCreate extends StatelessWidget {
+class ProductCreate extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreateState();
+  }
+}
+class _ProductCreateState extends State<ProductCreate>{
+  String title;
+  String description;
+  double price;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text("Save"),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text("This is a modal"),
-                );
-              });
-        },
-      ),
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child:ListView(children: <Widget>[
+      TextField(
+        decoration: InputDecoration(labelText: 'Product Title'),
+        onChanged: (String value){
+        setState(() {
+          title=value;
+        });
+      },),
+      TextField(
+        decoration: InputDecoration(labelText: 'Product Description'),
+        maxLines: 4,
+        onChanged: (String value){
+        setState(() {
+          description=value;
+        });
+      },),
+      TextField(
+        decoration: InputDecoration(labelText: 'Product Price'),
+        keyboardType: TextInputType.number,
+        onChanged: (String value){
+        setState(() {
+          price=double.parse(value);
+        });
+      },),
+    ],
+    ),
     );
   }
 }
