3502d77cf42f1a5972a980eb72dd23e7f9adaee3
Kunal Indoliya <kunalindoliya@gmail.com>
2019-01-20 23:51:42 +0530

    Improving List Tile.

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -6,23 +6,32 @@ class ProductList extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
 
-  ProductList(this.products,this.updateProduct);
+  ProductList(this.products, this.updateProduct);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          //leading: Image.asset(products[index]["image"]),
+        return Column(children: <Widget>[ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(products[index]['image']),
+          ),
           title: Text(products[index]['title']),
+          subtitle: Text("\$${products[index]['price'].toString()}"),
           trailing: IconButton(
               icon: Icon(Icons.edit),
               onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return ProductEdit(product:products[index],updateProduct: updateProduct,productIndex: index,);
-                    }));
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return ProductEdit(
+                    product: products[index],
+                    updateProduct: updateProduct,
+                    productIndex: index,
+                  );
+                }));
               }),
+        ),
+        Divider()],
         );
       },
       itemCount: products.length,
